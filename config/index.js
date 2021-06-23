e2358ea21e12ecb325f5c8b6a6ac209821832a24
Owen Young <theowenyoung@gmail.com>
2021-06-23 20:11:56 +0800

    feat: no static query

--- config/index.js
+++ config/index.js
@@ -8,6 +8,7 @@ const siteMetadata = {
   keywords: ["buzzing", "西方媒体", "国外新闻", "世界新闻", "突发新闻"],
   telegram: "@buzzingcc",
   telegram_chat_id: "-1001406368978",
+  iconUrl: "https://news.buzzing.cc/avatar.png",
   menuLinks: [
     // {
     //   name: "每周精选",
